3d468363e373951cf6b4bd66853c426d8be1ea8a
taaleksandra <94486752+taaleksandra@users.noreply.github.com>
2021-11-24 17:33:03 +0100

    Add files via upload

---
+++ AT_HOMEWORK/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AT_HOMEWORK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoje imię:");
+            string imie = Console.ReadLine();
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie}, wprowadź pierwszą liczbę:");
+            double wprowadzona_liczba = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie} wprowadź następną liczbę:");
+            double wprowadzona_liczba2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie}, suma Twoich liczb to: {wprowadzona_liczba + wprowadzona_liczba2}");
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie}, różnica Twoich liczb to: {wprowadzona_liczba - wprowadzona_liczba2}");
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie}, iloczyn Twoich liczb to: {wprowadzona_liczba * wprowadzona_liczba2}");
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine($"{imie}, iloraz Twoich liczb to: {wprowadzona_liczba / wprowadzona_liczba2}");
+
+            Console.WriteLine("\n\r");
+
+            Console.WriteLine("Naciśnij dowolny klawisz, aby zakońćzyć. \n\r" +
+                "Do zobaczenia!");
+
+        }
+    }
+}
